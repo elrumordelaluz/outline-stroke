e49d820e5d1f45dab8b58f44f74c9ee5b22d20da
Lionel T <elrumordelaluz@hotmail.com>
2017-12-01 16:28:14 +0100

    Clean code

--- index.js
+++ index.js
@@ -1,41 +1,7 @@
 const potrace = require('potrace')
 const svg2img = require('svg2img')
 
-// const convert = input => {
-//   const canvas = Canvas.createCanvas()
-//   canvg(canvas, input, {
-//     ignoreMouse: true,
-//     ignoreAnimation: true,
-//   })
-//   const buffer = canvas.toBuffer()
-//   return new Promise((resolve, reject) => {
-//     if (buffer) {
-//       potrace.trace(buffer, (err, svg) => {
-//         if (err) {
-//           reject(`Error in potrace: ${err}`)
-//         }
-//         resolve(svg)
-//       })
-//     } else {
-//       reject('No input')
-//     }
-//   })
-// }
-
-// const convert = input => {
-//   return svg2png(input).then(buffer => {
-//     return new Promise((resolve, reject) => {
-//       potrace.trace(buffer, (err, svg) => {
-//         if (err) {
-//           reject(`Error in potrace: ${err}`)
-//         }
-//         resolve(svg)
-//       })
-//     })
-//   })
-// }
-
-const convert = input => {
+const outlineStroke = input => {
   return new Promise((resolve, reject) => {
     svg2img(input, (err, buffer) => {
       if (err) {
@@ -51,4 +17,4 @@ const convert = input => {
   })
 }
 
-exports = module.exports = convert
+exports = module.exports = outlineStroke
