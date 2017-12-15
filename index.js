0567ffb823c0ba6062ef7445331bc57515c764c6
Lionel T <elrumordelaluz@hotmail.com>
2017-12-16 00:04:30 +0100

    Add params to pass to  potrace and relative tests

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const { promisify } = require('util')
 
 const trace = promisify(potrace.trace)
 
-const outlineStroke = input => {
+const outlineStroke = (input, params) => {
   const src = Buffer.isBuffer(input) ? input : Buffer.from(input)
   return sharp(src)
     .toBuffer()
-    .then(trace)
+    .then(buff => trace(buff, params))
 }
 
 exports = module.exports = outlineStroke
